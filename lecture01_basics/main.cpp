0d22c9e3c5cf99aa1c8a268c6f346398534f65f9
k1r1ll19 <kirya.t163@gmail.com>
2025-09-21 21:21:50 +0200

    ADD: Hello, world, cmake

---
+++ lecture01_basics/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>//iostream-библиотека ввода и вывода
+
+// fjfjfjfj
+/*
+111
+222
+333
+*/
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;//std::cout - для вывода        std::endl - перенос строки
+    std::cout << "111" << std::endl;
+    return 0;
+}
