4a9c8f04fa23cb278bb8770f0fd78456531e8db2
k1r1ll19 <kirya.t163@gmail.com>
2025-10-11 12:01:14 +0200

    ADD: lab01

--- lab01/main.cpp
+++ lab01/main.cpp
@@ -15,14 +15,7 @@ int main(){
 
     std::cout << "\nСередина интервала: " << middle << std::endl;
 
-    /*
-    std::cout << "\nТип данных: double" << std::endl;
-    int bits = sizeof(double) * 8;
-    int minValue = -pow(2, bits - 1);
-    int maxValue = pow(2, bits - 1) - 1;
-    
-    std::cout << "Минимальное значение double: " << minValue << std::endl;
-    std::cout << "Максимальное значение double: " << maxValue << std::endl;
-    */
+    std::cout << "double занимает: " << sizeof(double) * 8 << "бит(а)" << std::endl;
+    std::cout << "примерный диапазон = от -1.7e308 до 1.7e308" << std::endl;
     return 0;
 }
