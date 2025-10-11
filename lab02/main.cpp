52b76e164058fe4badeab1ff3b38b3ae0f37676e
k1r1ll19 <kirya.t163@gmail.com>
2025-10-11 12:01:06 +0200

    ADD:lab02

---
+++ lab02/main.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <bitset>
+#include <cmath>
+
+
+int main() {
+    // ======== ПУНКТ 1 ========
+    short A;
+    int i;
+
+    std::cout << "Введите число A (тип short): ";
+    std::cin >> A;
+
+    std::cout << "Введите номер бита i (от 0 до 7): ";
+    std::cin >> i;
+
+    // Проверка правильности ввода i
+    if (i < 0 || i > 7) {
+        std::cout << "Ошибка: номер бита должен быть от 0 до 7." << std::endl;
+        return 0;
+    }
+
+    int bit_i = (A >> i) & 1;//извлекаем i-й бит
+
+    std::cout << "Число A до изменений: " << A << " = " << std::bitset<8>(A) << std::endl;
+    std::cout << "i-й бит (" << i << ") равен: " << bit_i << std::endl;
+
+    if (bit_i == 0) {
+        // Меняем местами i-й и 0-й биты
+        int bit0 = A & 1; // 0-й бит
+
+        if (bit0 != bit_i) {
+            A ^= (1 << i); // поменяли i-й бит
+            A ^= 1;        // поменяли 0-й бит
+        }
+
+        std::cout << "После обмена битов: " << A << " = " << std::bitset<8>(A) << std::endl;
+    } 
+    else {
+        // Если i-й бит равен 1 — работаем с тремя числами
+        int x, y, z;
+        std::cout << "Введите три целых числа: ";
+        std::cin >> x >> y >> z;
+
+        int ax = abs(x);
+        int ay = abs(y);
+        int az = abs(z);
+
+        // Проверяем равенство модулей
+        if (ax == ay || ay == az || ax == az) {
+            std::cout << "Код 0. Вычисление невозможно." << std::endl;
+        } 
+        else {
+            // Находим среднее по модулю
+            int maxAbs = ax;
+            if (ay > maxAbs)
+                maxAbs = ay;
+            if (az > maxAbs)
+                maxAbs = az;
+
+            int minAbs = ax;
+            if (ay < minAbs)
+                minAbs = ay;
+            if (az < minAbs)
+                minAbs = az;
+
+            int middle;
+
+            if ((ax != maxAbs) && (ax != minAbs))
+                middle = x;
+            else if ((ay != maxAbs) && (ay != minAbs)) 
+                middle = y;
+            else
+                middle = z;
+
+            std::cout << "Среднее по модулю число: " << middle << std::endl;
+        }
+    }
+
+    // ======== ПУНКТ 2 ========
+    int N;
+    std::cout << "\nВведите номер региона: ";
+    std::cin >> N;
+
+    switch (N) {
+        case 102: 
+        case 116: 
+        case 716:
+            std::cout << "Республика Татарстан" << std::endl; break;
+        case 11: std::cout << "Республика Коми" << std::endl; break;
+        case 13: std::cout << "Республики Мордовия" << std::endl; break;
+        case 15: std::cout << "Республика Северная Осетия" << std::endl; break;
+        
+        default: std::cout << "Информация временно недоступна." << std::endl; break;
+        
+    }
+
+    return 0;
+}
+
+//102, 11, 13, 15, 116, 716.
